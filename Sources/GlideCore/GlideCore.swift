5cc83e3f57fe3a66e89b21648762c6f0c6f3a8d8
Pranay Venkat Aluri <145622254+helloGithub326@users.noreply.github.com>
2026-07-02 23:46:50 -0500

    Previous commit was a mistake. I forgot how to use git for a bit and did quite a bit of digging to get back my previous changes.

--- Sources/GlideCore/GlideCore.swift
+++ Sources/GlideCore/GlideCore.swift
@@ -1,2 +1,27 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+public enum ParsedLine {
+    case task(text: String, time: String?, checked: Bool, recurrence: Recurrence)
+    case note(text: String)
+}
+
+public enum Recurrence {
+    case daily
+    case weekly
+    case weekday
+    case monthly
+    case specificDay(Weekday)
+    case none
+}
+
+public enum Weekday {
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+    case saturday
+    case sunday
+}
+
+public func parseLine(_ line: String) -> ParsedLine {
+    return ParsedLine.note(text: line)
+}
